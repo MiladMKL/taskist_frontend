800715b3f49f5da146cf62a105478049a2fb8338
Milad <milad@MBP-von-Milad.fritz.box>
2022-11-12 17:46:50 +0100

    Before Part 4

--- src/services/tasks.js
+++ src/services/tasks.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 // const baseUrl = "http://localhost:3001/api/tasks"
-const baseUrl = "https://fullstack-taskist-clone.herokuapp.com/api/tasks"
-// const baseUrl = "/api/tasks"
+// const baseUrl = "https://fullstack-taskist-clone.herokuapp.com/api/tasks"
+const baseUrl = "/api/tasks"
 
 const getAll = () => {
 	const request = axios.get(baseUrl)
